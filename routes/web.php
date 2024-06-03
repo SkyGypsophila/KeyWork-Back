bfdf034522bc0e99c9e16a1b5c2429ca4e04ba3f
Daven <147076994+DavenUPF@users.noreply.github.com>
2024-06-03 15:59:13 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OfferController;
+use App\Http\Controllers\AuthenticateSessionController;
+use App\Http\Controllers\RegisterUserController;
 
-Route::get('/', function () {
+Route::get('/', function () { 
     return view('welcome');
 });
+
+// Routes for offers
+Route::get('/offers', [OfferController::class, 'showAll']);
+Route::get('/offers/id', [OfferController::class, 'showSelected']);
+Route::post('/create', [OfferController::class, 'create']);
+
+// Routes for users
+Route::get('/login', [AuthenticateSessionController::class, 'login']);
+Route::post('/signin', [RegisterUserController::class, 'signin']);
