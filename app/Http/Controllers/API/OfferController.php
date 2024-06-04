d87871cc5d18a9fd864a455b5d5125bc7c4dbadb
Mousemices <9.12716135xia@gmail.com>
2024-06-04 14:10:13 +0200

    [ADD] store method to create an offer by user. [UPDATE] Organize the structure of api.php.

--- app/Http/Controllers/API/OfferController.php
+++ app/Http/Controllers/API/OfferController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\API;
 
 use App\Models\Offer;
+use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class OfferController
 {
@@ -14,5 +16,28 @@ class OfferController
         return response()->json([
             'offers' => $offers,
         ]);
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+        $request->merge(['start_date' => Carbon::parse($request->input('start_date'))->format('Y-m-d H:i:s')]);
+        $request->merge(['end_date' => Carbon::parse($request->input('end_date'))->format('Y-m-d H:i:s')]);
+
+        $request->validate([
+           'title' => ['required', 'string', 'max:255'],
+           'description' => ['required', 'string'],
+           'price' => ['required', 'numeric'],
+           'start_date' => ['required', 'date_format:Y-m-d H:i:s'],
+           'end_date' => ['required', 'date_format:Y-m-d H:i:s', 'after:start_date'],
+        ]);
+
+        $offer = auth()->user()->offers()->create($request->only(
+            'title', 'description', 'price', 'start_date', 'end_date',
+        ));
+
+        return response()->json([
+            'offer' => $offer,
+        ]);
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,18 @@ use App\Http\Controllers\API\OfferController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+
+Route::middleware('auth:sanctum')->group(function() {
+    Route::post('/logout', [AuthenticateSessionController::class, 'destroy'])->name('api.auth.destroy');
+
+    Route::post('/offers/store', [OfferController::class, 'store'])->name('api.offer.store');
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    })->middleware('auth:sanctum');
+});
 
 Route::post('/login', [AuthenticateSessionController::class, 'store'])->name('api.auth.store');
-Route::post('/logout', [AuthenticateSessionController::class, 'destroy'])->middleware('auth:sanctum')->name('api.auth.destroy');
 
 Route::get('/offers', [OfferController::class, 'index'])->name('api.offers.index');
