6b826ecee24c71644e948f5060114d8874a30a2c
Mousemices <9.12716135xia@gmail.com>
2024-06-07 01:12:36 +0200

    [REMOVE] Unnecessary comments.

--- app/Http/Controllers/API/OfferSuggestionController.php
+++ app/Http/Controllers/API/OfferSuggestionController.php
@@ -11,18 +11,15 @@ class OfferSuggestionController extends Controller
 {
     protected const GOOGLE_API = 'https://generativelanguage.googleapis.com/';
 
-    // https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=$GOOGLE_API_KEY
-
     public function generate(Request $request, int $id): JsonResponse
     {
-        //$result = Http::get(self::GOOGLE_API . 'v1beta/models?key='. env('GEMINI_API_KEY'))->json();
         $offerTitle = $request->input('title');
 
         $result = Http::post(self::GOOGLE_API . 'v1beta/models/chat-bison-001:generateMessage?key='. env('GEMINI_API_KEY'), [
             'prompt' => [
                 'context' => 'You are great on answer question.',
                 'messages' => [
-                    'content' => 'Offer profile looking for this position:"' .$offerTitle .'" (max 50 words).',
+                    'content' => 'Offer profile looking for this position:"' . $offerTitle .'" (max 50 words).',
                 ]
             ]
         ])->json();
